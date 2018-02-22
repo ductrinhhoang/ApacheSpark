cefef1770d9b7c8f972b557f071195e5461591a0
DucTH <ductrinhhoang@github.com>
2018-02-22 16:00:21 +0700

    Finish Pair RDD

---
+++ spark-2.2.1-bin-hadoop2.7/python/trainning/DataPartitioning/pageRank.py
@@ -0,0 +1,30 @@
+from pyspark import SparkContext
+
+def resultFormat(res):
+    return float("{0:.2f}".format(res))
+
+def change(arr):
+    nArr = []
+    val = 1.0/len(arr)
+    for obj in arr:
+        nArr.append((obj,float(val)))
+    return nArr
+
+sc = SparkContext(appName="pageRank")
+
+data = [("facebook",["yahoo", "google","weibo"]),("yahoo",["facebook"]),("google",["facebook","weibo"]),("weibo",["facebook","google"])]
+
+rdd = sc.parallelize(data)
+
+rdd0 = rdd.mapValues(lambda x: change(x))\
+            .flatMap(lambda x: x[1])\
+            .reduceByKey(lambda x, y: x+y)\
+            .mapValues(lambda val: resultFormat(val))\
+            .sortByKey(True)
+            # change x -> (x[0],(x[1][i], len(x[1])))
+            # remove "[","]" via flatMap()
+            # combine result
+            # change and format result
+            # sort result
+
+print rdd0.collect()
