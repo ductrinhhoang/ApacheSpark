c836b96b5569358a2d4f0cfc3a57fc4680d62dd0
DucTH <ductrinhhoang@github.com>
2018-02-28 11:40:10 +0700

    hihi

--- spark-2.2.1-bin-hadoop2.7/python/trainning/DataPartitioning/pageRankv2.py
+++ spark-2.2.1-bin-hadoop2.7/python/trainning/DataPartitioning/pageRankv2.py
@@ -14,25 +14,23 @@ sc = SparkContext(appName="pageRank")
 
 data = [("facebook",["yahoo", "google","weibo"]),("yahoo",["facebook"]),("google",["facebook","weibo"]),("weibo",["facebook","google"])]
 
-# inp = sc.parallelize(data).partitionBy(4) #25s
+# inp = sc.parallelize(data).partitionBy(4) #20s
 
-inp = sc.parallelize(data) #37s
+inp = sc.parallelize(data) #27s
 
 rank = inp.mapValues(lambda val: 1)
 
 i=0
 while i<10:
-    rank = inp.union(rank)\
-                .reduceByKey(lambda x, y: [x, y])\
+    rank = inp.join(rank)\
                 .mapValues(lambda x: change(x[0], x[1]))\
                 .flatMap(lambda x: x[1])\
                 .reduceByKey(lambda x, y: x+y)\
                 .mapValues(lambda val: resultFormat(val))
-                # Ghep rdd
-                # Reduce (K, V) -> (K, (V, R))
-                # Mapvalues (K, [v1, ..., vj]) -> (K, [(v1, R/j),..., (v2, R/j)] )
-                # FlatMap (K, [(v1, R/j),..., (v2, R/j)]) -> (v1, R/j), ..., (vj, R/j)
-                # remove "[","]" via flatMap()
+                # Ghep (K, V), (K, R) -> (K, (V, R))
+                # Mapvalues (K, ([v1, ..., vj], R)) -> (K, [(v1, R/j),..., (v2, R/j)] )
+                # FlatMap (K, [(v1, R/j),..., (vj, R/j)]) -> (v1, R/j), ..., (vj, R/j)
+		
     i = i+1
 
 print rank.collect()
